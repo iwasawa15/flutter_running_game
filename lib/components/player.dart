339b8a5ccb3af8c599c76c2861d828e6cf9028b1
iwasawa15 <tatsuya57sak@gmail.com>
2022-08-28 15:10:45 +0900

    [add]: player component

---
+++ lib/components/player.dart
@@ -0,0 +1,34 @@
+import 'package:flame/cache.dart';
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+
+class Player extends SpriteAnimationComponent {
+  final double _playerSpeed = 300.0;
+  final double _animationSpeed = 0.15;
+  late final SpriteAnimation _idleAnimation;
+  late final SpriteAnimation _runAnimation;
+
+  Player()
+      : super(
+          size: Vector2.all(50.0),
+        );
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    _loadAnimations().then((_) => {animation = _idleAnimation});
+  }
+
+  Future<void> _loadAnimations() async {
+    final spriteSheet = SpriteSheet.fromColumnsAndRows(
+        image: await Images().load('doux_spritesheet.png'),
+        rows: 1,
+        columns: 24);
+
+    _idleAnimation = spriteSheet.createAnimation(
+        row: 0, stepTime: _animationSpeed, from: 0, to: 4);
+
+    _runAnimation = spriteSheet.createAnimation(
+        row: 0, stepTime: _animationSpeed, from: 4, to: 10);
+  }
+}
